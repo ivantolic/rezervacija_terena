017073b6e39e31d5a3966f8ee044011ba70a5158
tolic <ivan081169@gmail.com>
2024-09-05 12:33:57 +0200

    Add reservation for each pitch

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import Footer from './components/Footer'; // Import 
+import Footer from './components/Footer'; 
 import Header from './components/Header';
 import Reservation from './components/Reservation';
+import ReservationTeren1 from './components/ReservationTeren1'; // Komponenta za Teren 1
+import ReservationTeren2 from './components/ReservationTeren2'; // Komponenta za Teren 2
+import ReservationTeren3 from './components/ReservationTeren3'; // Komponenta za Teren 3
 
 function App() {
   return (
     <div className="app">
       <Header />
+      
       {/* Glavni dio aplikacije sa rutama */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/rezervacija" element={<Reservation />} />
-        {/* Ovdje možeš dodati više ruta */}
+        <Route path="/rezervacija" element={<Reservation />} />  {/* Prikazuje izbor terena */}
+        <Route path="/rezervacija-teren1" element={<ReservationTeren1 />} /> {/* Stranica za rezervaciju terena 1 */}
+        <Route path="/rezervacija-teren2" element={<ReservationTeren2 />} /> {/* Stranica za rezervaciju terena 2 */}
+        <Route path="/rezervacija-teren3" element={<ReservationTeren3 />} /> {/* Stranica za rezervaciju terena 3 */}
       </Routes>
 
-      {/* Footer je ispod svih ruta */}
       <Footer />
     </div>
   );
